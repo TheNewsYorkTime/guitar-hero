8a381225a7450f4f1fc710a8ea93246193794880
TheNewsYorkTime <asalawless5c@gmail.com>
2025-03-25 23:42:28 -0700

    fix audio (missing capital causing 404)

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import React, { useState, useEffect, useRef } from "react";
 import Cookies from "js-cookie";
 import "./game.css";
 import chordArray from "./chords.js";
-const turistaAudio4sec = "/turistaAudio4sec.mp3";
+const turistaAudio4sec = "/turistaAudio4Sec.mp3";
 const turistaAudio2 = "/turistaAudio2.mp3";
 
 const stringX = [50, 100, 150, 200, 350, 400, 450, 500];
